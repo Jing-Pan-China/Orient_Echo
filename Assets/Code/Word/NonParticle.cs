aaf4f1656af8d34693e967d9d3d559e64e4f12ba
Jing Pan <jingpan0004@gmail.com>
2026-01-09 15:20:18 +0800

    Add basic inventory pickup

---
+++ Assets/Code/Word/NonParticle.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Non_Particle")]
+public class NonParticle : ScriptableObject
+{
+   public string text;
+
+
+  
+}
---
+++ Assets/Code/WordData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Non_Particle")]
+public class Non_Particle : ScriptableObject
+{
+   public string nonparticle;
+}
